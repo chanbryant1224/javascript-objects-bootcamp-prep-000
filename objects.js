0563aabaadaa37fc72129bbe1a03bafda193feb4
Wing Chak Chan <chanbryant1224@gmail.com>
2017-11-18 21:57:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,3 @@
 var playlist = {artistName: "songTitle"};
 function updatePlaylist(playlist, artistName, songTitle){return Object.assign({},playlist,{[artistName]:songTitle})}
-function removeFromPlaylist(playlist, artistName){ 
+function removeFromPlaylist(playlist, artistName){ delete playlist.[artistName]
